3cbe0dd63d42eaf31b87daaee30074320d9ffa5f
Antonio Alvarez <antonio_wata@ciencias.unam.mx>
2017-02-25 20:34:15 -0600

    Add files via upload

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -24,19 +24,40 @@ public class Controlador {
         return "inicio";
     }
     
-    @RequestMapping(value="/persona1", method = RequestMethod.GET)
-    public ModelAndView persona1(ModelMap model,HttpServletRequest request){
-        String p = request.getParameter("nombre1");
-        model.addAttribute("persona", p);
-        return new ModelAndView("persona",model);
-    
-    }
-    
     @RequestMapping(value="/persona2", method = RequestMethod.POST)
     public ModelAndView persona2(ModelMap model,HttpServletRequest request){
-        String p = request.getParameter("nombre2");
-        model.addAttribute("persona", p);
+        String corr = request.getParameter("nombre2");
+        String cont = request.getParameter("nombre3");
+        model.addAttribute("correo", corr);
+        model.addAttribute("contrasena", cont);
         return new ModelAndView("persona",model);
     
     }
+    
+    
+    
+    @RequestMapping(value="/persona1?", method = RequestMethod.GET)
+    public ModelAndView persona1(ModelMap model,HttpServletRequest request){
+        String p = request.getParameter("nombre1");
+        String c = request.getParameter("carrera1");
+        String d = request.getParameter("date1");
+        String e = request.getParameter("email1");
+        String pas = request.getParameter("password1");
+        model.addAttribute("nombre1", p);
+        model.addAttribute("carrera1", c);
+        model.addAttribute("date1", d);
+        model.addAttribute("email1", e);
+        model.addAttribute("password1", pas);
+        return new ModelAndView("registro_completo",model);
+    
+    }
+    
+    
+    @RequestMapping(value="/registro1", method = RequestMethod.GET)
+    public ModelAndView registro1(ModelMap model,HttpServletRequest request){
+        return new ModelAndView("registro",model);
+    
+    }
+    
+    
 }
